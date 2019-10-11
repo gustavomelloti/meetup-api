bc54ddae0934e6c90af7d94176496c49702f4bac
Gustavo Mello <gustavomelloti@gmail.com>
2019-10-11 11:52:58 -0300

    Adicionando tratamento de erros

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import path from 'path';
-import routes from './routes';
+import Youch from 'youch';
 
+import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -22,6 +25,17 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
