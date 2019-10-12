c5fd8575dbb1710fb553f5849df6bb515166acb4
Gustavo Mello <gustavomelloti@gmail.com>
2019-10-12 16:04:28 -0300

    Ajustes no mapeamento do banner do meetup

--- src/app/controllers/ResponsibleController.js
+++ src/app/controllers/ResponsibleController.js
@@ -1,10 +1,20 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class ResponsbileController {
   async index(req, res) {
     const user_id = req.userId;
 
-    const meetups = await Meetup.findAll({ where: { user_id } });
+    const meetups = await Meetup.findAll({
+      include: [
+        {
+          model: File,
+          as: 'banner',
+        },
+      ],
+      where: { user_id },
+      order: [['id', 'DESC']],
+    });
 
     return res.json(meetups);
   }
--- src/app/models/Meetup.js
+++ src/app/models/Meetup.js
@@ -19,7 +19,7 @@ class Meetup extends Model {
 
   static associate(models) {
     this.hasMany(models.Subscription, { foreignKey: 'meetup_id' });
-    this.belongsTo(models.File, { foreignKey: 'banner_id' });
+    this.belongsTo(models.File, { foreignKey: 'banner_id', as: 'banner' });
     this.belongsTo(models.User, { foreignKey: 'user_id' });
   }
 }
